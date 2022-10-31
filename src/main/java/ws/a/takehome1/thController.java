553da1a19b73bd8d91b2da378d3f2bd30fd88cec
Administrator <Administrator@WIN-171A8TGB90A>
2022-10-31 17:54:35 +0700

    commit 4 Signed-off-by:Administrator <Administrator@WIN-171A8TGB90A>

---
+++ src/main/java/ws/a/takehome1/thController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ws.a.takehome1;
+
+/**
+ *
+ * @author Administrator
+ */
+public class thController {
+    
+}
