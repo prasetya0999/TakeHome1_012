8ac61804b5887e01e8500433c5958b40820e8b52
Administrator <Administrator@WIN-171A8TGB90A>
2022-10-31 18:13:56 +0700

    commit 6 Signed-off-by:Administrator <Administrator@WIN-171A8TGB90A>

--- src/main/java/ws/a/takehome1/thController.java
+++ src/main/java/ws/a/takehome1/thController.java
@@ -4,7 +4,13 @@
  */
 package ws.a.takehome1;
 
+import java.io.IOException;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -13,5 +19,20 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class thController {
+    @RequestMapping("nextpage")
+    public String next(@RequestParam(value="nm") String isiPertama,
+            @RequestParam(value="lok") String isiKedua,
+            @RequestParam(value="pic") MultipartFile isiKetiga,
+            Model kurir)
+            throws IOException{
+        byte[] img = isiKetiga.getBytes();
+        String base64Image = Base64.encodeBase64String(img);
+        String imglink ="data:image/png;base64,".concat(base64Image);
+        kurir.addAttribute("paket1", isiPertama);
+        kurir.addAttribute("paket2", isiKedua);
+        kurir.addAttribute("paket3", imglink);
+        
+        return "pageview";
+    }
     
 }
