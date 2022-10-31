aa14d21a7a2fc52ffb2d60f2a821527648127180
Administrator <Administrator@WIN-171A8TGB90A>
2022-10-31 17:41:11 +0700

    commite 1 Signed-off-by:Administrator <Administrator@WIN-171A8TGB90A>

---
+++ src/main/java/ws/a/takehome1/Takehome1Application.java
@@ -0,0 +1,13 @@
+package ws.a.takehome1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Takehome1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Takehome1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ws/a/takehome1/Takehome1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.a.takehome1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Takehome1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
